1532258a70d22b9b8f5aa804004bf0672d3c0737
Kevin Goos <kevingoos@telenet.be>
2017-02-07 10:50:37 +0100

    Updated project file

--- GPS.NET/Ghostware.GPS.NET/GpsClients/BaseGpsClient.cs
+++ GPS.NET/Ghostware.GPS.NET/GpsClients/BaseGpsClient.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ghostware.GPS.NET.GpsClients
+﻿namespace Ghostware.GPS.NET.GpsClients
 {
     public abstract class BaseGpsClient
     {
