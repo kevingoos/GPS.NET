24b471356c96143c1994f5b28bc38f8b9b22bdc6
Kevin Goos <kevingoos@telenet.be>
2017-02-08 12:08:01 +0100

    Extracted the proxy handler

---
+++ GPS.NET/Ghostware.GPS.NET/Handlers/ProxyClientHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+
+namespace Ghostware.GPS.NET.Handlers
+{
+    public static class ProxyClientHandler
+    {
+        public static TcpClient GetTcpClient(string serverAddress, string serverPort, string proxyAddress, int proxyPort, bool isProxyAuthenticationEnabled = false, string proxyUsername = "", string proxyPassword = "")
+        {
+            var uriBuilder = new UriBuilder
+            {
+                Scheme = Uri.UriSchemeHttp,
+                Host = proxyAddress,
+                Port = proxyPort
+            };
+
+            var proxyUri = uriBuilder.Uri;
+            var request = WebRequest.Create("http://" + serverAddress + ":" + serverPort);
+            var webProxy = new WebProxy(proxyUri);
+
+            request.Proxy = webProxy;
+            request.Method = "CONNECT";
+
+            if (isProxyAuthenticationEnabled)
+            {
+                webProxy.Credentials = new NetworkCredential(proxyUsername, proxyPassword);
+            }
+            else
+            {
+                webProxy.UseDefaultCredentials = true;
+            }
+
+            var response = request.GetResponse();
+            var responseStream = response.GetResponseStream();
+            Debug.Assert(responseStream != null);
+
+            const BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Instance;
+
+            var rsType = responseStream.GetType();
+            var connectionProperty = rsType.GetProperty("Connection", flags);
+
+            var connection = connectionProperty.GetValue(responseStream, null);
+            var connectionType = connection.GetType();
+            var networkStreamProperty = connectionType.GetProperty("NetworkStream", flags);
+
+            var networkStream = networkStreamProperty.GetValue(connection, null);
+            var nsType = networkStream.GetType();
+            var socketProperty = nsType.GetProperty("Socket", flags);
+            var socket = (Socket)socketProperty.GetValue(networkStream, null);
+
+            return new TcpClient { Client = socket };
+        }
+    }
+}
